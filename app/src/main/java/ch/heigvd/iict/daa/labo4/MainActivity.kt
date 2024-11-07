42fa3e0447325e9336e52ee5feef544f67e65439
Loïc Herman <loic@loicherman.ch>
2024-11-07 13:59:33 +0100

    feat: add menu items and inflater

--- app/src/main/java/ch/heigvd/iict/daa/labo4/MainActivity.kt
+++ app/src/main/java/ch/heigvd/iict/daa/labo4/MainActivity.kt
@@ -1,11 +1,32 @@
 package ch.heigvd.iict.daa.labo4
 
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import ch.heigvd.iict.daa.labo4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // Bindings to the activity content
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // TODO
+        return super.onOptionsItemSelected(item)
     }
 }
